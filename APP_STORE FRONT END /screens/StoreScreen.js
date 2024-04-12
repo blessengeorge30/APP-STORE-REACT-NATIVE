6287ed0bc2a2f30e4decfd2b21e6baae59572dd6
Blessen George <blessen.george30@gmail.com>
2024-04-12 22:55:25 +0530

    updated

--- APP_STORE FRONT END /screens/StoreScreen.js
+++ APP_STORE FRONT END /screens/StoreScreen.js
@@ -134,7 +134,7 @@ export default function StoreScreen() {
                 <Text
                   style={{color: storeColors.text}}
                   className="ml-4 text-lg font-bold">
-                    Featured Games yo!
+                    Featured Games 
                   </Text>
                 <View className="pl-4">
                   <ScrollView horizontal showsHorizontalScrollIndicator={false}>
